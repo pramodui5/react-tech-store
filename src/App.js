281b8bc67de34b3e1fb34c3afbe7854a831b1dba
pramodui5 <mail.pramod.bhoi@gmail.com>
2019-04-09 21:33:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Hello from tech store</h1>
+    );
+  }
+}
+
+export default App;
